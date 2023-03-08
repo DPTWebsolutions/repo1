36b806b8cac9287344bc036293643d28acc944dd
DPTWebsolutions <127328653+DPTWebsolutions@users.noreply.github.com>
2023-03-08 16:42:17 +0000

    Create TTDownload.py

---
+++ TTDownload.py
@@ -0,0 +1,10 @@
+<form>
+  <label for="video-url">Enter video URL:</label>
+  <input type="text" id="video-url" name="video-url">
+
+  <button id="convert-button" type="button">Convert</button>
+
+  <br>
+
+  <progress id="progress-bar" value="0" max="100"></progress>
+</form>
